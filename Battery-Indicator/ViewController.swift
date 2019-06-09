e053104880bbe4ae188f4e2e4f361b431da4c865
Kai Gothe <kaigostudio@gmail.com>
2019-06-09 11:42:07 +0200

    Initial Commit

---
+++ Battery-Indicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Battery-Indicator
+//
+//  Created by Kai Gothe on 09.06.19.
+//  Copyright © 2019 Kai Gothe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
